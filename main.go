733c56a7ffd0c5c52d941169809fc7a9d20c8f7c
delucks <ping@jamieluck.com>
2020-03-27 00:07:34 -0400

    Working challenge and response

--- main.go
+++ main.go
@@ -6,9 +6,12 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 )
 
 type MinecraftServer struct {
@@ -19,10 +22,11 @@ func (s MinecraftServer) Connect() {
 }
 
 func WriteCommand(sock *net.UDPConn, command byte, body []byte) ([]byte, error) {
-	fullPayload := []byte{0xfe, 0xfd, command, 0x01, 0x02, 0x03, 0x04}
+	fullPayload := []byte{0xfe, 0xfd, command, 0x00, 0x00, 0x00, 0x01}
 	if body != nil {
 		fullPayload = append(fullPayload, body...)
 	}
+	fmt.Printf("Request: %v\n", fullPayload)
 	_, err := sock.Write(fullPayload)
 	if err != nil {
 		return nil, err
@@ -33,8 +37,19 @@ func WriteCommand(sock *net.UDPConn, command byte, body []byte) ([]byte, error) 
 		return nil, err
 	}
 	response := buffer[0:nBytes]
+	if len(response) < 5 || response[0] != command {
+		return nil, fmt.Errorf("Response %v was too short or didn't match with command %v\n", response, command)
+	}
 	fmt.Printf("Reply: %v\n", response)
-	return response, nil
+	return response[5:], nil
+}
+
+func swapByteOrder(in []byte) []byte {
+	for i := len(in)/2 - 1; i >= 0; i-- {
+		opp := len(in) - 1 - i
+		in[i], in[opp] = in[opp], in[i]
+	}
+	return in
 }
 
 func main() {
@@ -54,7 +69,29 @@ func main() {
 	errorPipe := make(chan error, 1)
 	go func() {
 		// Handshake
-		_, err := WriteCommand(conn, 0x09, nil)
+		response, err := WriteCommand(conn, 0x09, nil)
+		if err != nil {
+			errorPipe <- err
+			return
+		}
+		// Parse challenge token from the response
+		nonNullTerminated := string(response[:len(response)-1])
+		parsedToken, err := strconv.ParseInt(nonNullTerminated, 10, 32)
+		if err != nil {
+			errorPipe <- err
+			return
+		}
+		buf := new(bytes.Buffer)
+		// convert to int32 to correct the binary size
+		err = binary.Write(buf, binary.BigEndian, int32(parsedToken))
+		if err != nil {
+			errorPipe <- err
+			return
+		}
+		packedToken := buf.Bytes()
+		// Must be padded with four null bytes
+		challengeResponse := append(packedToken, []byte{0x00, 0x00, 0x00, 0x00}...)
+		_, err = WriteCommand(conn, 0x00, challengeResponse)
 		if err != nil {
 			errorPipe <- err
 			return
